0fe92e8aab0b39179ee5109d85361b48895d9d3c
Praveen K Paladugu <prapal@linux.microsoft.com>
2026-08-31 21:35:46 +0000

    optee: Introduce RPC definitions
    Introduce RPC definitions required to handle responses for RPC Commands.
    Introduce Shm types used during RPC calls.
    
    Signed-off-by: Praveen K Paladugu <prapal@linux.microsoft.com>

--- litebox_common_optee/src/lib.rs
+++ litebox_common_optee/src/lib.rs
@@ -1437,6 +1437,22 @@ const OPTEE_MSG_RPC_CMD_RPMB_PROBE_RESET: u32 = 22;
 const OPTEE_MSG_RPC_CMD_RPMB_PROBE_NEXT: u32 = 23;
 const OPTEE_MSG_RPC_CMD_RPMB_PROBE_FRAMES: u32 = 24;
 
+/// Memory that can be shared with a non-secure user space application
+const OPTEE_RPC_SHM_TYPE_APPL: u32 = 0;
+/// Memory only shared with non-secure kernel
+const OPTEE_RPC_SHM_TYPE_KERNEL: u32 = 1;
+/// Memory shared with non-secure kernel and exported to a non-secure user
+/// space application
+const OPTEE_RPC_SHM_TYPE_GLOBAL: u32 = 2;
+
+/// OP-TEE RPC shared memory types
+#[repr(u32)]
+pub enum OpteeRpcShmType {
+    Appl = OPTEE_RPC_SHM_TYPE_APPL,
+    Kernel = OPTEE_RPC_SHM_TYPE_KERNEL,
+    Global = OPTEE_RPC_SHM_TYPE_GLOBAL,
+}
+
 /// RPC command IDs from `optee_os/core/include/optee_msg.h`
 ///
 /// These are the command IDs used in the `cmd` field of the RPC `optee_msg_arg`.
@@ -2327,6 +2343,7 @@ impl OpteeSmcArgs {
 /// TODO: Add stuffs based on the OP-TEE driver that LVBS is using.
 const OPTEE_SMC_FUNCID_GET_OS_UUID: usize = 0x0;
 const OPTEE_SMC_FUNCID_GET_OS_REVISION: usize = 0x1;
+const OPTEE_SMC_FUNCID_RETURN_FROM_RPC: usize = 0x3;
 const OPTEE_SMC_FUNCID_CALL_WITH_ARG: usize = 0x4;
 const OPTEE_SMC_FUNCID_EXCHANGE_CAPABILITIES: usize = 0x9;
 const OPTEE_SMC_FUNCID_DISABLE_SHM_CACHE: usize = 0xa;
@@ -2341,6 +2358,7 @@ const OPTEE_SMC_FUNCID_CALLS_REVISION: usize = 0xff03;
 pub enum OpteeSmcFunction {
     GetOsUuid = OPTEE_SMC_FUNCID_GET_OS_UUID,
     GetOsRevision = OPTEE_SMC_FUNCID_GET_OS_REVISION,
+    ReturnFromRpc = OPTEE_SMC_FUNCID_RETURN_FROM_RPC,
     CallWithArg = OPTEE_SMC_FUNCID_CALL_WITH_ARG,
     ExchangeCapabilities = OPTEE_SMC_FUNCID_EXCHANGE_CAPABILITIES,
     DisableShmCache = OPTEE_SMC_FUNCID_DISABLE_SHM_CACHE,
@@ -2388,6 +2406,11 @@ pub enum OpteeSmcResult<'a> {
     CallWithArg {
         msg_args: Box<OpteeMsgArgs>,
         rpc_args: Option<Box<OpteeRpcArgs>>,
+        msg_args_phys_addr: u64,
+    },
+    ReturnFromRpc {
+        msg_args: Box<OpteeMsgArgs>,
+        rpc_args: Box<OpteeRpcArgs>,
         msg_args_phys_addr: u64,
     },
 }
@@ -2451,6 +2474,11 @@ impl From<OpteeSmcResult<'_>> for OpteeSmcArgs {
             OpteeSmcResult::CallWithArg { .. } => {
                 panic!(
                     "OpteeSmcResult::CallWithArg cannot be converted to OpteeSmcArgs directly. Handle the incorporated OpteeMsgArgs."
+                );
+            }
+            OpteeSmcResult::ReturnFromRpc { .. } => {
+                panic!(
+                    "OpteeSmcResult::ReturnFromRpc cannot be converted to OpteeSmcArgs directly. Handle the incorporated OpteeMsgArgs and OpteeRpcArgs."
                 );
             }
         }
